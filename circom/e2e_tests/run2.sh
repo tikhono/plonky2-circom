78c81b969daf0dea7012af2ef6f6a1dc18354003
tikhono <tikhonov.andrii.m@gmail.com>
2023-07-27 15:18:06 +0300

    ...

--- circom/e2e_tests/run2.sh
+++ circom/e2e_tests/run2.sh
@@ -1,11 +1,11 @@
 CIRCUIT_NAME=plonky2
 CIRCUIT_PATH=../circuits/plonky2.circom
 INPUT_PATH=../test/data/proof.json
-POT_PATH=~/Downloads/powersOfTau28_hez_final_25.ptau
+POT_PATH=../../powersOfTau28_hez_final_25.ptau
 RAPIDSNARK_PATH=../../../../rapidsnark/build/prover
 # node version > 18
 NODE_PATH=node
-SNARKJS_PATH=../../../../snarkjs/cli.js
+SNARKJS_PATH="$(which snarkjs)"
 NODE_PARAMS="--trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc"
 
 echo "****WITNESS GENERATION****"
